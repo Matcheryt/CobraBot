ab334587dfe21e026e29f015cd88c18c6ade0432
Telmo Duarte <matcheryt@gmail.com>
2018-02-22 18:05:03 +0000

    Create Helpers.cs

---
+++ Helpers/Helpers.cs
@@ -0,0 +1,31 @@
+namespace CobraBot.Helpers
+{
+    public class Helpers
+    {
+        //Not much to say here
+
+        //Verify if string contains only numbers
+        public bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        //Uppercase first letter of a string
+        public string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+    }
+}
