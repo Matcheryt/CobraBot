34516b2a1c04f589a863bc18e96c5b3f9a1c76dc
Telmo Duarte <matcheryt@gmail.com>
2018-02-22 18:06:05 +0000

    Add files via upload

---
+++ Modules/Comandos.cs
@@ -0,0 +1,189 @@
+﻿using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+using System;
+using Discord.WebSocket;
+
+namespace CobraBot.Modules
+{
+    public class Comandos : ModuleBase<SocketCommandContext>
+    {
+        //Default error builder
+        EmbedBuilder errorBuilder = new EmbedBuilder().WithColor(Color.Red);
+
+        //Defines game bot is currently Playing
+        [RequireOwner]
+        [Command("setbotgame")]
+        public async Task SetGame(string game)
+        {
+            if (game == null)
+                return;
+
+            if (!(Context.User.Id == <YourDiscordIdHere>))
+            {
+                return;
+            }
+            else
+            {
+                await (Context.Client as DiscordSocketClient).SetGameAsync(game);
+                Console.WriteLine($"{DateTime.Now}: Game was changed to {game}");
+            }
+
+        }
+
+        //Random number between minVal and maxVal
+        [Command("random")]
+        public async Task RandomNumber(int minVal = 0, int maxVal = 0)
+        {
+            #region ErrorHandling
+            if (minVal > maxVal)
+            {
+                errorBuilder.WithDescription("**Minimum Value cannot be greater than Maximum Value**");
+                await ReplyAsync("", false, errorBuilder.Build());
+                return;
+            }
+
+            if (minVal >= 2147483647 || maxVal >= 2147483647)
+            {
+                errorBuilder.WithDescription("**Value cannot be greater than 2147483647**");
+                await ReplyAsync("", false, errorBuilder.Build());
+                return;
+            }
+            else if (minVal <= -2147483647 || maxVal <= -2147483647)
+            {
+                errorBuilder.WithDescription("**Value cannot be lesser than -2147483647**");
+                await ReplyAsync("", false, errorBuilder.Build());
+                return;
+            }
+            #endregion
+
+            try
+            {
+                Random r = new Random();
+                int randomNumber = r.Next(minVal, maxVal + 1);
+                await ReplyAsync(":game_die: Random number is: " + randomNumber);
+            }
+            catch(Exception)
+            {
+                errorBuilder.WithDescription("**An error ocurred** Please check command syntax.");
+            }         
+        }
+
+        //Shows help
+        [Command("help")]
+        public async Task Help()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Cobra Commands")
+                .WithDescription(
+                "**Misc**" +
+                "\n-random (minNumber, maxNumber) - Gets a random number between minNumber and maxNumber" +
+                "\n-help - Shows this help message" +
+                "\n-usinfo (@User) - Shows info about the mentioned user" +
+                "\n-clean (numberOfMessages) - Cleans messages from chat with specified number" +
+                "\n-lmgtfy (text) - Creates a lmgtfy link about text inputed" +
+                "\n-dict (word) - UNDER CONSTRUCTION" +
+                "\n-steam (id) - Shows Steam profile Info for a specific SteamID" +
+                "\n-weather (city) - Shows current weather for specific city\n\n" +
+                "**Music (Non functional)**" +
+                "\n-stream (online stream url) - Streams music from Online Stream" +
+                "\n-play (youtubeUrl) - Plays video from Youtube URL" +
+                "\n-stop - Stops audio streaming and makes bot leave channel")
+                .WithColor(Color.Red)
+                .WithThumbnailUrl(Context.Client.CurrentUser.GetAvatarUrl());
+
+            await Context.User.SendMessageAsync("", false, builder.Build());
+        }
+
+        //Clean messages
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [Command("clean", RunMode = RunMode.Async)]
+        public async Task LimparMensagens(int count = 1)
+        {
+            if (count <= 100)
+            {
+                /*Tenta guardar todas as mensagens que o utilizador que apagar
+                 * numa variavel, de seguida apaga todas as mensagens que o utilizador escolheu
+                 * e envia uma mensagem para o textChannel a dizer que X mensagens foram apagadas,
+                 * sendo a mesma apagada 2.3s depois*/
+                try
+                {
+                    var messagesToDelete = await Context.Channel.GetMessagesAsync(count + 1).FlattenAsync();
+                    var textChannelId = Context.Channel.Id;
+                    await Context.Guild.GetTextChannel(Context.Channel.Id).DeleteMessagesAsync(messagesToDelete);
+                    var tempMessage = await Context.Channel.SendMessageAsync("Deleted " + "**" + count + "**" + " messages :white_check_mark:");
+                    var lastMessageId = tempMessage.Id;
+
+                    await Task.Delay(2300);
+                    await tempMessage.DeleteAsync();
+                }
+                catch(Exception)
+                {
+                    errorBuilder.WithDescription("**Something went wrong!** Please try again");
+                    await ReplyAsync("", false, errorBuilder.Build());
+                }               
+            }
+            else
+            {
+                await ReplyAsync(Context.User.Mention + " You cannot delete more than 100 messages");
+            }
+            
+        }
+
+        //Show discord user info
+        [Command("usinfo")]
+        public async Task GetInfo(IGuildUser user = null)
+        {
+            if (user == null)
+            {
+                errorBuilder.WithDescription("**Please specify a user**");
+                await ReplyAsync("", false, errorBuilder.Build());
+            }
+            else
+            {
+                var application = await Context.Client.GetApplicationInfoAsync();
+                var thumbnailUrl = user.GetAvatarUrl();
+                var date = $"{user.CreatedAt.Day}/{user.CreatedAt.Month}/{user.CreatedAt.Year}";
+                var joinedAt = $"{user.JoinedAt.Value.Day}/{user.JoinedAt.Value.Month}/{user.JoinedAt.Value.Year}";
+
+
+                var author = new EmbedAuthorBuilder()
+                {
+                    Name = user.Username,
+                    IconUrl = thumbnailUrl,
+                };
+
+                var embed = new EmbedBuilder()
+                {
+                    Color = new Color(29, 140, 209),
+                    Author = author
+                };
+
+                var us = user as SocketGuildUser;
+                var username = us.Username;
+                var discr = us.Discriminator;
+                var id = us.Id;
+                var dat = date;
+                var stat = us.Status;
+                var joinedServer = joinedAt;
+                var game = us.Activity;
+                var nick = us.Nickname;
+                embed.Title = $"{username} Info";
+                embed.Description = $"Username: **{username}**\n"
+                    + $"Discriminator: **{discr}**\n"
+                    + $"User ID: **{id}**\n"
+                    + $"Nickname: **{nick}**\n"
+                    + $"Created At: **{date}**\n"
+                    + $"Current Status: **{stat}**\n"
+                    + $"Joined Server At: **{joinedServer}**\n"
+                    + $"Playing: **{game}**";
+                embed.WithThumbnailUrl(thumbnailUrl);
+
+                await ReplyAsync("", false, embed.Build());
+            }
+        }
+       
+    }
+}
---
+++ Modules/MusicModule.cs
@@ -0,0 +1,162 @@
+﻿using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord.Audio;
+using System.Net;
+using System.IO;
+using System.Linq;
+using CobraBot.Services;
+using System;
+
+namespace CobraBot.Modules
+{
+    public class MusicModule : ModuleBase<SocketCommandContext>
+    {
+
+        /* Don't know if this is working fine, because of new Discord.NET version
+         * also I haven't worked on this for a while because my bot is hosted on Ubuntu
+         * and the services required for the music to function properly, only work on Windows
+           Be sure you host your bot on Windows Server or just install Linux version of the services */
+
+        //Music Service service
+        private MusicService musicService;
+
+        public MusicModule(MusicService _musicService)
+        {
+            musicService = _musicService;
+        }
+
+        //Play command
+        [Command("play", RunMode = RunMode.Async)]
+        public async Task PlayCmd([Remainder]string url)
+        {
+
+            var channel = (Context.Message.Author as IGuildUser).VoiceChannel;
+
+            //User needs role to be able to play music
+            var user = Context.User as SocketGuildUser;
+            var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "DJ");
+
+            if (!user.Roles.Contains(role))
+            {
+                await ReplyAsync(":no_entry: Insufficient Permission");
+            }
+            else
+            {
+                if (channel == null)
+                {
+                    await ReplyAsync(":no_entry_sign: You need to be in a voice channel!");
+                }
+                else
+                {
+                    if (url.ToLower().Contains("youtube.com"))
+                    {
+                        //Joins channel
+                        IVoiceChannel vchannel = (Context.User as IVoiceState).VoiceChannel;
+                        IAudioClient client = await vchannel.ConnectAsync();
+                        musicService.audioDict.TryAdd(Context.Guild.Id, client);
+
+                        EmbedBuilder builder = new EmbedBuilder();
+
+                        string thumbnailYoutube = url.Substring(url.IndexOf("=") + 1);
+
+                        //Send an embed with the information of the video requested
+                        builder.WithTitle("**Video Requested**")
+                            .WithDescription("Playing: " +  musicService.GetInfoFromYouTube(url).Result + "\n\n")
+                            .WithColor(Color.Green)
+                            .WithFooter("Requested By: " + user.Username)
+                            .WithThumbnailUrl("http://img.youtube.com/vi/" + thumbnailYoutube + "/default.jpg");
+
+                        await ReplyAsync("", false, builder.Build());
+
+                        //Creates stream
+                        var output = musicService.StreamYoutube(url).StandardOutput.BaseStream;
+                        var stream = client.CreatePCMStream(AudioApplication.Music, 128 * 1024);
+                        await output.CopyToAsync(stream);
+                        await stream.FlushAsync().ConfigureAwait(false);
+
+                        //await StopCmd(Context, vchannel);
+
+                    }
+                    else
+                    {
+                        await ReplyAsync("You need to specify a **Youtube URL**");
+                    }
+                }
+            }
+        }
+        
+        //Streams audio from an online radio based on URL
+        [Command("stream", RunMode = RunMode.Async)]
+        public async Task Cacadora([Remainder]string url)
+        {
+            if (url == null)
+                return;
+
+            var channel = (Context.Message.Author as IGuildUser).VoiceChannel;
+
+            //User needs role to be able to play music
+            var user = Context.User as SocketGuildUser;
+            var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "DJ");
+
+            if (!user.Roles.Contains(role))
+            {
+                await ReplyAsync(":no_entry: Insufficient Permission");
+            }
+            else
+            {
+                if (channel == null)
+                {
+                    await ReplyAsync(":no_entry_sign: You need to be in a voice channel!");
+                }
+                else
+                {
+                    await ReplyAsync(":exclamation: Please note that if you paste the wrong URL, the bot will still join the channel but play no sound! Make sure you paste the right online stream url. :exclamation:");
+
+                    IVoiceChannel vchannel = (Context.User as IVoiceState).VoiceChannel;
+                    IAudioClient client = await channel.ConnectAsync();
+                    musicService.audioDict.TryAdd(Context.Guild.Id, client);
+
+                    //Creates a request based on online radio url
+                    WebRequest request = WebRequest.Create(url);
+                    WebResponse response = await request.GetResponseAsync();
+
+                    using (Stream streamResponse = response.GetResponseStream())
+                    {
+                        var output = musicService.CreateStream(url).StandardOutput.BaseStream;
+                        var stream = client.CreatePCMStream(AudioApplication.Music, 128 * 1024);
+                        await output.CopyToAsync(stream);
+                        streamResponse.CopyTo(stream);
+                        stream.FlushAsync().Wait();
+                    }
+                }
+            }
+        }
+
+        //Stop command
+        [Command("stop", RunMode = RunMode.Async)]
+        public async Task StopCommand()
+        {
+            var user = Context.User as SocketGuildUser;
+            var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "DJ");
+            var channel = (Context.Message.Author as IGuildUser)?.VoiceChannel;
+
+            if (channel == null)
+            {
+                await Context.Channel.SendMessageAsync(":no_entry_sign: You need to be in a voice channel!");
+                return;
+            }
+
+            if (!user.Roles.Contains(role))
+            {
+                await Context.Channel.SendMessageAsync(":no_entry: Insufficient Permission");
+                return;
+            }
+
+            await musicService.StopCmd(Context, channel);
+
+        }
+
+    }
+}
