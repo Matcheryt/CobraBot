31ac8dc6a8596451f8e56a9674dcbf382ad9f007
Telmo <contact@telmoduarte.me>
2020-11-26 21:22:37 +0000

    Add moderation module
    - Added moderation module
    - Added new table to database to store the channel that will display user join/leave messages
    - Renamed AudioService to MusicService

--- CobraBot/Modules/CommandsModule.cs
+++ CobraBot/Modules/CommandsModule.cs
@@ -3,7 +3,6 @@ using Discord;
 using System.Threading.Tasks;
 using System;
 using CobraBot.Helpers;
-using CobraBot.Handlers;
 
 namespace CobraBot.Modules
 {
@@ -16,43 +15,7 @@ namespace CobraBot.Modules
         {
             await Context.Client.SetGameAsync(game);
             Console.WriteLine($"{DateTime.Now}: Game was changed to {game}");
-        }
-
-        //Sets custom prefix
-        [RequireUserPermission(GuildPermission.Administrator)]
-        [Command("prefix")]
-        public async Task SetPrefix(string prefix)
-        {
-            //If user input == default
-            if (prefix == "default")
-            {
-                //Check if the guild has custom prefix
-                string currentPrefix = DatabaseHandler.GetPrefix(Context.Guild.Id);
-
-                //If the guild doesn't have custom prefix, return
-                if (currentPrefix == null)
-                {
-                    await ReplyAsync(embed: await Helper.CreateErrorEmbed("Bot prefix is already the default one!"));
-                    return;
-                }
-
-                //If they have a custom prefix, remove it from database and consequently setting it to default
-                DatabaseHandler.RemovePrefixFromDB(Context.Guild.Id);
-                await ReplyAsync(embed: await Helper.CreateBasicEmbed("", "Bot prefix was reset to:  **-**", Color.DarkGreen));
-                return;
-            }
-
-            //If user input is longer than 5, return
-            if (prefix.Length > 5)
-            {
-                await ReplyAsync(embed: await Helper.CreateErrorEmbed("Bot prefix can't be longer than 5 characters!"));
-                return;
-            }
-
-            //If every check passes, we add the new custom prefix to the database
-            DatabaseHandler.AddPrefixToDB(Context.Guild.Id, prefix);
-            await ReplyAsync(embed: await Helper.CreateBasicEmbed("Prefix Changed", $"Bot's prefix is now:  **{prefix}**", Color.DarkGreen));
-        }
+        }     
 
         //Random number between minVal and maxVal
         [Command("random")]
@@ -118,46 +81,7 @@ namespace CobraBot.Modules
                 .WithColor(Color.DarkGreen);
 
             await ReplyAsync("", false, helpMessage.Build());
-        }
-
-        //Clean messages
-        [RequireBotPermission(GuildPermission.ManageMessages)]
-        [RequireUserPermission(GuildPermission.ManageMessages)]
-        [Command("clean", RunMode = RunMode.Async)]
-        public async Task LimparMensagens(int count = 1)
-        {
-            //We only delete 100 messages at a time to prevent bot from getting overloaded
-            if (count <= 100)
-            {
-                /* Saves all messages user specified in a variable, next
-                   those messages are deleted and a message is sent to the textChannel
-                   saying that X messages were deleted <- this message is deleted 2.3s later */
-                try
-                {
-                    //Save messages to delete in a variable
-                    var messagesToDelete = await Context.Channel.GetMessagesAsync(count + 1).FlattenAsync();
-                    
-                    //Delete messages to delete
-                    await Context.Guild.GetTextChannel(Context.Channel.Id).DeleteMessagesAsync(messagesToDelete);
-                    
-                    //Message sent informing that X messages were deleted
-                    var tempMessage = await Context.Channel.SendMessageAsync("Deleted " + "**" + count + "**" + " messages :white_check_mark:");            
-
-                    //Delete temp message after 2.3s
-                    await Task.Delay(2300);
-                    await tempMessage.DeleteAsync();
-                }
-                catch(Exception)
-                {
-                    await ReplyAsync(embed: await Helper.CreateErrorEmbed("**Something went wrong!** Please try again"));
-                }               
-            }
-            else
-            {
-                await ReplyAsync(embed: await Helper.CreateErrorEmbed(Context.User.Mention + " You cannot delete more than 100 messages at once"));
-            }
-            
-        }
+        }      
 
         //Show discord user info
         [Command("usinfo")]
---
+++ CobraBot/Modules/ModerationModule.cs
@@ -0,0 +1,63 @@
+﻿using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+using CobraBot.Services;
+
+namespace CobraBot.Modules
+{
+    [RequireContext(ContextType.Guild)]
+    public class ModerationModule : ModuleBase<SocketCommandContext>
+    {
+        public ModerationService ModerationService { get; set; }
+
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [Command("ban")]
+        public async Task BanUser(IUser user, int pruneDays = 0, [Remainder] string reason = null)
+            => await ReplyAsync(embed: await ModerationService.BanAsync(user, pruneDays, reason, Context));
+
+
+        //[RequireBotPermission(GuildPermission.BanMembers)]
+        //[RequireUserPermission(GuildPermission.BanMembers)]
+        //[Command("unban")]
+        //public async Task UnbanUser(IUser bannedUser)
+        //    => await ReplyAsync(embed: await ModerationService.UnbanAsync(bannedUser, Context));
+
+
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        [Command("kick")]
+        public async Task Kick(IGuildUser user, [Remainder] string reason = null)
+            => await ReplyAsync(embed: await ModerationService.KickAsync(user, reason, Context));
+
+
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [Command("clean", RunMode = RunMode.Async)]
+        public async Task LimparMensagens(int count = 1)
+        {
+            var message = await ReplyAsync(embed: await ModerationService.CleanMessagesAsync(count, Context));
+            await Task.Delay(2300);
+            await message.DeleteAsync();
+        }
+
+
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        [Command("role")]
+        public async Task UpdateUserRole(IGuildUser user, char operation, [Remainder]string roleName)
+            => await ReplyAsync(embed: await ModerationService.UpdateRoleAsync(user, operation, roleName));
+
+
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [Command("prefix")]
+        public async Task SetPrefix(string prefix)
+            => await ReplyAsync(embed: await ModerationService.ChangePrefixAsync(prefix, Context));
+
+
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [Command("setwelcome")]
+        public async Task SetWelcomeChannel(ITextChannel textChannel)
+            => await ReplyAsync(embed: await ModerationService.SetWelcomeChannel(textChannel));
+    }
+}
