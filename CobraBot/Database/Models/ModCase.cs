b96b7763b1e15f9171b364cdde5c3c6601772d2f
Telmo Duarte <contact@telmoduarte.me>
2021-01-25 02:16:31 +0000

    License update (#34)
    * Update copyright notices
    
    * Update LICENSE

--- CobraBot/Database/Models/ModCase.cs
+++ CobraBot/Database/Models/ModCase.cs
@@ -1,4 +1,23 @@
-﻿using Discord;
+﻿#region License
+/*CitizenEnforcer - Moderation and logging bot
+Copyright(C) 2018-2020 Hawx
+https://github.com/Hawxy/CitizenEnforcer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.If not, see http://www.gnu.org/licenses/ */
+#endregion
+
+using Discord;
 using Discord.Commands;
 using System;
 using System.ComponentModel.DataAnnotations;
